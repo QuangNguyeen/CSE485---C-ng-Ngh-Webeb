a1b06788a06673c1eb056caf9adc7b5e9debd1ff
QuangNguyeen <nguyenvanquangngunguoi@gmail.com>
2024-12-09 15:15:28 +0700

    Update TH3

--- BTTH/TH03_09-12/seeders/ComputersTableSeeder.php
+++ BTTH/TH03_09-12/seeders/ComputersTableSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
+use App\Models\Computer;
 class ComputersTableSeeder extends Seeder
 {
     /**
@@ -12,6 +13,16 @@ class ComputersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        for($i = 0; $i < 10; $i++) {
+            Computer::create([
+                'computer_name' => $faker->company,
+                'model' => $faker->company,
+                'operating_system' => $faker->company,
+                'processor' => $faker->company,
+                'memory' => $faker->randomElement([4, 8, 16, 32, 64,128,256,512]),
+                'available' => $faker->randomElement([true, false]),
+            ]);
+        }
     }
 }
--- BTTH/TH03_09-12/seeders/IssuesTableSeeder.php
+++ BTTH/TH03_09-12/seeders/IssuesTableSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Computer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
+use App\Models\Issue;
 class IssuesTableSeeder extends Seeder
 {
     /**
@@ -12,6 +14,17 @@ class IssuesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        $computerId = Computer::pluck('id')->toArray();
+        for($i = 0; $i < 10; $i++) {
+            Issue::create([
+                'computer_id' => $faker->randomElement($computerId),
+                'reported_by' => $faker->firstName,
+                'reported_date' => $faker->date(),
+                'description' => $faker->sentence(),
+                'urgency' => $faker->randomElement(['Low', 'Medium', 'High']),
+                'status' => $faker->randomElement(['Open', 'In Process', 'Resolved']),
+            ]);
+        }
     }
 }
